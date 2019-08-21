26c7e5d937ae95765dbb8e4b32b31afe923f5021
Max Trawinski <makstr@users.noreply.github.com>
2019-08-21 13:25:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8080;
 
 const requestHandler = (request, response) => {
   console.log(request.url);
-  response.end("Hello World, I'm Node.js v4!");
+  response.end("Hello World, I'm Node.js v5!");
 };
 
 const server = http.createServer(requestHandler);
